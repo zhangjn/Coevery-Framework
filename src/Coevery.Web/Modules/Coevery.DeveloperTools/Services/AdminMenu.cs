581195f0ce3ff59eddbb7650a773dd4f73a05b12
Zhang Haoran <icese7en@gmail.com>
2014-03-11 11:43:44 +0800

    Fix path error

--- src/Coevery.Web/Modules/Coevery.DeveloperTools/Services/AdminMenu.cs
+++ src/Coevery.Web/Modules/Coevery.DeveloperTools/Services/AdminMenu.cs
@@ -15,17 +15,17 @@ namespace Coevery.DeveloperTools.Services {
         }
 
         public void GetNavigation(NavigationBuilder builder) {
-            builder.Add(T("Home"), "1", menu => menu.Url("~/"), new[] { "icon-home" });
+            builder.Add(T("Home"), "1", menu => menu.Url(""), new[] { "icon-home" });
 
             builder.AddImageSet("Entities")
                 .Add(T("Entities"), "2",
                     menu => {
                         int menuIdex = 0;
-                        menu.Add(T("All Entities"), (++menuIdex).ToString(), item => item.Url("~/SystemAdmin#/Entities"));
+                        menu.Add(T("All Entities"), (++menuIdex).ToString(), item => item.Url("DeveloperTools#/Entities"));
                         var userDefinedTypes = _contentDefinitionManager.GetRawEntities();
                         foreach (var type in userDefinedTypes) {
                             var typeModel = type;
-                            menu.Add(T(typeModel.DisplayName), (++menuIdex).ToString(), item => item.Url("~/SystemAdmin#/Entities/" + typeModel.Name));
+                            menu.Add(T(typeModel.DisplayName), (++menuIdex).ToString(), item => item.Url("DeveloperTools#/Entities/" + typeModel.Name));
                         }
                     }, new[] {"icon-list"});
         }
