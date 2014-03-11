da6d48f1862a44ca427da7c04dea788be8d91284
Zhang Haoran <icese7en@gmail.com>
2014-03-11 15:28:30 +0800

    Update path error

--- src/Coevery.Web/Modules/Coevery.DeveloperTools/Controllers/FieldAdminController.cs
+++ src/Coevery.Web/Modules/Coevery.DeveloperTools/Controllers/FieldAdminController.cs
@@ -6,194 +6,36 @@ using Coevery.ContentManagement;
 using Coevery.ContentManagement.MetaData;
 using Coevery.ContentManagement.MetaData.Models;
 using Coevery.DeveloperTools.Services;
-using Coevery.DeveloperTools.Settings;
 using Coevery.DeveloperTools.ViewModels;
 using Coevery.Localization;
 using Coevery.Logging;
-using Coevery.Themes;
 using Coevery.UI.Notify;
 using Coevery.Utility.Extensions;
 
 namespace Coevery.DeveloperTools.Controllers {
-    public class AdminController : Controller, IUpdateModel {
+    public class FieldAdminController : Controller, IUpdateModel {
         private readonly IContentDefinitionService _contentDefinitionService;
         private readonly IContentDefinitionEditorEvents _contentDefinitionEditorEvents;
         private readonly IContentMetadataService _contentMetadataService;
         private readonly ISettingService _settingService;
-        private readonly IEntityRecordEditorEvents _entityRecordEditorEvents;
 
-        public AdminController(
+        public FieldAdminController(
             ICoeveryServices coeveryServices,
             ISettingService settingService,
             IContentDefinitionService contentDefinitionService,
             IContentDefinitionEditorEvents contentDefinitionEditorEvents,
-            IContentMetadataService contentMetadataService,
-            IEntityRecordEditorEvents entityRecordEditorEvents) {
+            IContentMetadataService contentMetadataService) {
             Services = coeveryServices;
             _settingService = settingService;
             _contentDefinitionService = contentDefinitionService;
             T = NullLocalizer.Instance;
             _contentDefinitionEditorEvents = contentDefinitionEditorEvents;
             _contentMetadataService = contentMetadataService;
-            _entityRecordEditorEvents = entityRecordEditorEvents;
         }
 
         public ICoeveryServices Services { get; private set; }
         public Localizer T { get; set; }
         public ILogger Logger { get; set; }
-
-        [Themed]
-        public ActionResult Index(string returnUrl) {
-            ViewBag.ReturnUrl = returnUrl;
-            return View();
-        }
-
-        #region Entity Methods
-
-        public ActionResult List(string returnUrl) {
-            ViewBag.ReturnUrl = returnUrl;
-            return View();
-        }
-
-        public ActionResult Create() {
-            if (!Services.Authorizer.Authorize(Permissions.EditContentTypes, T("Not allowed to create a content type."))) {
-                return new HttpUnauthorizedResult();
-            }
-            var entityRecords = _entityRecordEditorEvents.FieldSettingsEditor().ToList();
-            var fieldTypes = entityRecords.Select(x =>
-                new SelectListItem {Text = x.FieldTypeDisplayName, Value = x.FieldTypeName});
-
-            var typeViewModel = _contentDefinitionService.GetType(string.Empty);
-            typeViewModel.Settings.Add("CollectionName", string.Empty);
-            typeViewModel.Settings.Add("CollectionDisplayName", string.Empty);
-            typeViewModel.FieldTypes = fieldTypes;
-            typeViewModel.FieldTemplates = entityRecords;
-
-            return View(typeViewModel);
-        }
-
-        public ActionResult EntityName(string displayName, int version) {
-            return Json(new {
-                result = _contentMetadataService.ConstructEntityName(displayName.ToSafeName()),
-                version = version
-            });
-        }
-
-        public ActionResult Publish(string id) {
-            var entity = _contentMetadataService.GetEntity(id);
-            Services.ContentManager.Publish(entity.ContentItem);
-            return new HttpStatusCodeResult(HttpStatusCode.OK);
-        }
-
-        [HttpPost, ActionName("Create")]
-        public ActionResult CreatePost(EditTypeViewModel viewModel) {
-            if (!Services.Authorizer.Authorize(Permissions.EditContentTypes, T("Not allowed to create a content type."))) {
-                return new HttpUnauthorizedResult();
-            }
-
-            viewModel.DisplayName = string.IsNullOrWhiteSpace(viewModel.DisplayName) ? String.Empty : viewModel.DisplayName.Trim();
-            viewModel.Name = (viewModel.Name ?? viewModel.DisplayName).ToSafeName();
-
-            viewModel.FieldLabel = string.IsNullOrWhiteSpace(viewModel.FieldLabel) ? String.Empty : viewModel.FieldLabel.Trim();
-            viewModel.FieldName = (viewModel.FieldName ?? viewModel.FieldLabel).ToSafeName();
-
-            if (String.IsNullOrWhiteSpace(viewModel.DisplayName)) {
-                ModelState.AddModelError("DisplayName", T("The Display Name name can't be empty.").ToString());
-            }
-
-            if (String.IsNullOrWhiteSpace(viewModel.Name)) {
-                ModelState.AddModelError("Name", T("The Content Type Id can't be empty.").ToString());
-            }
-
-            if (String.IsNullOrWhiteSpace(viewModel.FieldLabel)) {
-                ModelState.AddModelError("DisplayName", T("The Field Label name can't be empty.").ToString());
-            }
-
-            if (String.IsNullOrWhiteSpace(viewModel.FieldName)) {
-                ModelState.AddModelError("Name", T("The Field Name can't be empty.").ToString());
-            }
-
-            if (String.IsNullOrWhiteSpace(viewModel.FieldType)) {
-                ModelState.AddModelError("Name", T("The FieldType can't be empty.").ToString());
-            }
-
-            if (!_contentMetadataService.CheckEntityCreationValid(viewModel.Name, viewModel.DisplayName, viewModel.Settings)) {
-                ModelState.AddModelError("Name", T("A type with the same Name or DisplayName already exists.").ToString());
-            }
-
-            if (!ModelState.IsValid) {
-                Services.TransactionManager.Cancel();
-                Response.StatusCode = (int) HttpStatusCode.BadRequest;
-                var temp = (from values in ModelState
-                    from error in values.Value.Errors
-                    select error.ErrorMessage).ToArray();
-                return Content(string.Concat(temp));
-            }
-
-            _contentMetadataService.CreateEntity(viewModel, this);
-            return Json(new {entityName = viewModel.Name});
-        }
-
-        public ActionResult Edit(string id) {
-            if (!Services.Authorizer.Authorize(Permissions.EditContentTypes, T("Not allowed to edit a content type."))) {
-                return new HttpUnauthorizedResult();
-            }
-
-            var entity = _contentMetadataService.GetEntity(id);
-
-            if (entity == null) {
-                return HttpNotFound();
-            }
-            var viewModel = new EditTypeViewModel {
-                Name = entity.Name,
-                DisplayName = entity.DisplayName,
-                Settings = entity.EntitySetting
-            };
-            return View(viewModel);
-        }
-
-        [HttpPost, ActionName("Edit")]
-        public ActionResult EditPost(string id, EditTypeViewModel viewModel) {
-            if (!Services.Authorizer.Authorize(Permissions.EditContentTypes, T("Not allowed to edit a content type."))) {
-                return new HttpUnauthorizedResult();
-            }
-            var entity = _contentMetadataService.GetDraftEntity(id);
-
-            if (entity == null) {
-                return HttpNotFound();
-            }
-            bool valid = _contentMetadataService.CheckEntityDisplayValid(id, viewModel.DisplayName, viewModel.Settings);
-            if (!valid) {
-                return new HttpStatusCodeResult(HttpStatusCode.MethodNotAllowed);
-            }
-            entity.DisplayName = viewModel.DisplayName;
-            entity.EntitySetting = viewModel.Settings;
-            return new HttpStatusCodeResult(HttpStatusCode.OK);
-        }
-
-        public ActionResult Detail(string id) {
-            if (!Services.Authorizer.Authorize(Permissions.EditContentTypes, T("Not allowed to edit a content type."))) {
-                return new HttpUnauthorizedResult();
-            }
-
-            var entity = _contentMetadataService.GetEntity(id);
-
-            if (entity == null) {
-                return HttpNotFound();
-            }
-
-            var viewModel = new EntityDetailViewModel {
-                Id = entity.Id,
-                Name = entity.Name,
-                DisplayName = entity.DisplayName,
-                HasPublished = entity.HasPublished(),
-                Settings = entity.EntitySetting
-            };
-
-            return View(viewModel);
-        }
-
-        #endregion
 
         #region Field Methods
 
--- src/Coevery.Web/Modules/Coevery.DeveloperTools/Services/ClientRouteProvider.cs
+++ src/Coevery.Web/Modules/Coevery.DeveloperTools/Services/ClientRouteProvider.cs
@@ -4,36 +4,30 @@ namespace Coevery.DeveloperTools.Services {
     public class ClientRouteProvider : ClientRouteProviderBase {
         public override void Discover(ClientRouteTableBuilder builder) {
             builder.Describe("EntityList")
-                .Configure(descriptor => {
-                    descriptor.Url = "/Entities";
-                })
+                .Configure(descriptor => { descriptor.Url = "/Entities"; })
                 .View(view => {
-                    view.TemplateUrl = "'DeveloperTools/Admin/List'";
+                    view.TemplateUrl = "'DeveloperTools/EntityAdmin/List'";
                     view.Controller = "EntityListCtrl";
                     view.AddDependencies(ToAbsoluteScriptUrl, "controllers/listcontroller");
                 });
 
             builder.Describe("EntityCreate")
-                .Configure(descriptor => {
-                    descriptor.Url = "/Entities/Create";
-                })
+                .Configure(descriptor => { descriptor.Url = "/Entities/Create"; })
                 .View(view => {
-                    view.TemplateUrl = "'" + ModuleBasePath + @"Create'";
+                    view.TemplateUrl = "'DeveloperTools/EntityAdmin/Create'";
                     view.Controller = "EntityEditCtrl";
                     view.AddDependencies(ToAbsoluteScriptUrl, "controllers/editcontroller");
                 });
 
             builder.Describe("EntityEdit")
-                .Configure(descriptor => {
-                    descriptor.Url = "/Entities/{Id:[0-9a-zA-Z]+}/Edit";
-                })
+                .Configure(descriptor => { descriptor.Url = "/Entities/{Id:[0-9a-zA-Z]+}/Edit"; })
                 .View(view => {
                     view.TemplateProvider = @"['$http', '$stateParams', function ($http, $stateParams) {
-                                                var url = '" + ModuleBasePath + @"Edit/' + $stateParams.Id; 
+                                                var url = 'DeveloperTools/EntityAdmin/Edit/' + $stateParams.Id; 
                                                 return $http.get(url).then(function(response) { return response.data; });
                                           }]";
                     view.Controller = "EntityEditCtrl";
-                    view.AddDependencies(ToAbsoluteScriptUrl, new[] { "controllers/editcontroller" });
+                    view.AddDependencies(ToAbsoluteScriptUrl, new[] {"controllers/editcontroller"});
                 });
 
             builder.Describe("EntityDetail")
@@ -43,63 +37,55 @@ namespace Coevery.DeveloperTools.Services {
                 })
                 .View(view => {
                     view.TemplateProvider = @"['$http', '$stateParams', function ($http, $stateParams) {
-                                                var url = '" + ModuleBasePath + @"Detail/' + $stateParams.Id; 
+                                                var url = 'DeveloperTools/EntityAdmin/Detail/' + $stateParams.Id; 
                                                 return $http.get(url).then(function(response) { return response.data; });
                                           }]";
                     view.Controller = "EntityDetailCtrl";
-                    view.AddDependencies(ToAbsoluteScriptUrl, new[] { "controllers/detailcontroller" });
+                    view.AddDependencies(ToAbsoluteScriptUrl, new[] {"controllers/detailcontroller"});
                 });
 
             builder.Describe("EntityDetail.Fields")
                 .View(view => {
-                    view.TemplateUrl = "'" + ModuleBasePath + @"Fields'";
+                    view.TemplateUrl = "'DeveloperTools/EntityAdmin/Fields/'";
                     view.Controller = "FieldsCtrl";
-                    view.AddDependencies(ToAbsoluteScriptUrl, new[] { "controllers/fieldscontroller" });
+                    view.AddDependencies(ToAbsoluteScriptUrl, new[] {"controllers/fieldscontroller"});
                 });
 
             #region Operate fields
 
             builder.Describe("EntityDetail.Fields.Create")
-                .Configure(descriptor => {
-                    descriptor.Url = "/Create";
-                })
+                .Configure(descriptor => { descriptor.Url = "/Create"; })
                 .View(view => {
-                    view.TemplateUrl = "function(params) { return '" + ModuleBasePath + @"ChooseFieldType/' + params.Id; }";
+                    view.TemplateUrl = "function(params) { return 'DeveloperTools/FieldAdmin/ChooseFieldType/' + params.Id; }";
                     view.Controller = "ChooseFieldTypeCtrl";
-                    view.AddDependencies(ToAbsoluteScriptUrl, new[] { "controllers/choosefieldtypecontroller" });
+                    view.AddDependencies(ToAbsoluteScriptUrl, new[] {"controllers/choosefieldtypecontroller"});
                 });
 
             builder.Describe("EntityDetail.Fields.CreateFillInfo")
-                .Configure(descriptor => {
-                    descriptor.Url = "/Create/{FieldTypeName:[0-9a-zA-Z]+}";
-                    })
+                .Configure(descriptor => { descriptor.Url = "/Create/{FieldTypeName:[0-9a-zA-Z]+}"; })
                 .View(view => {
-                    view.TemplateUrl = "function(params) { return '" + ModuleBasePath + @"FillFieldInfo/' + params.Id + '?FieldTypeName=' + params.FieldTypeName; }";
+                    view.TemplateUrl = "function(params) { return 'DeveloperTools/FieldAdmin/FillFieldInfo/' + params.Id + '?FieldTypeName=' + params.FieldTypeName; }";
                     view.Controller = "FillFieldInfoCtrl";
-                    view.AddDependencies(ToAbsoluteScriptUrl, new[] { "controllers/fillfieldinfocontroller" });
+                    view.AddDependencies(ToAbsoluteScriptUrl, new[] {"controllers/fillfieldinfocontroller"});
                 });
 
             builder.Describe("EntityDetail.Fields.CreateConfirmInfo")
-               .Configure(descriptor => {
-                   descriptor.Url = "/Create/{FieldTypeName:[0-9a-zA-Z]+}/Confirm";
-               })
-               .View(view => {
-                   view.TemplateUrl = "function(params) { return '" + ModuleBasePath + @"ConfirmFieldInfo/' + params.Id; }";
-                   view.Controller = "ConfirmFieldInfoCtrl";
-                   view.AddDependencies(ToAbsoluteScriptUrl, new[] { "controllers/confirmfieldinfocontroller" });
-               });
+                .Configure(descriptor => { descriptor.Url = "/Create/{FieldTypeName:[0-9a-zA-Z]+}/Confirm"; })
+                .View(view => {
+                    view.TemplateUrl = "function(params) { return 'DeveloperTools/FieldAdmin/ConfirmFieldInfo/' + params.Id; }";
+                    view.Controller = "ConfirmFieldInfoCtrl";
+                    view.AddDependencies(ToAbsoluteScriptUrl, new[] {"controllers/confirmfieldinfocontroller"});
+                });
 
             builder.Describe("FieldEdit")
-                .Configure(descriptor => {
-                    descriptor.Url = "/Fields/{EntityName:[0-9a-zA-Z]+}/Edit/{FieldName:[0-9a-zA-Z]+}";
-                })
+                .Configure(descriptor => { descriptor.Url = "/Fields/{EntityName:[0-9a-zA-Z]+}/Edit/{FieldName:[0-9a-zA-Z]+}"; })
                 .View(view => {
                     view.TemplateProvider = @"['$http', '$stateParams', function ($http, $stateParams) {
-                                                var url = '" + ModuleBasePath + @"EditFields/' + $stateParams.EntityName + '?FieldName=' + $stateParams.FieldName;
+                                                var url = 'DeveloperTools/FieldAdmin/EditFields/' + $stateParams.EntityName + '?FieldName=' + $stateParams.FieldName;
                                                 return $http.get(url).then(function(response) { return response.data; });
                                           }]";
                     view.Controller = "FieldEditCtrl";
-                    view.AddDependencies(ToAbsoluteScriptUrl, new[] { "controllers/editfieldscontroller" });
+                    view.AddDependencies(ToAbsoluteScriptUrl, new[] {"controllers/editfieldscontroller"});
                 });
 
             #endregion
