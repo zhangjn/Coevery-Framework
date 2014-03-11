98888c59ed16bf02abd167d24ef08b6f3df13d47
Zhang Junnan <zhangjn0430@gmail.com>
2014-03-11 10:22:13 +0800

    Add FieldController

---
+++ src/Coevery.Web/Modules/Coevery.DeveloperTools/Controllers/FieldController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Coevery.DeveloperTools.Services;
+using Coevery.DeveloperTools.Settings;
+using Coevery.Localization;
+using Coevery.Utility.Extensions;
+
+namespace Coevery.DeveloperTools.Controllers {
+    public class FieldController : ApiController {
+        private readonly IContentMetadataService _contentMetadataService;
+        private readonly ISettingService _settingService;
+
+        public FieldController(
+            ISettingService settingService,
+            IContentMetadataService contentMetadataService) {
+            _settingService = settingService;
+            _contentMetadataService = contentMetadataService;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        // GET api/metadata/field
+        public object Get(int name, int page, int rows) {
+            var metadataTypes = _contentMetadataService.GetFieldsList(name);
+
+            var query = from field in metadataTypes
+                let fieldType = field.ContentFieldDefinitionRecord.Name
+                let setting = _settingService.ParseSetting(field.Settings)
+                select new {
+                    field.Name,
+                    field.Id,
+                    DisplayName = setting["DisplayName"],
+                    FieldType = fieldType.CamelFriendly(),
+                    Type = setting.GetModel<FieldSettings>(fieldType + "Settings").IsSystemField
+                        ? "System Field" : "User Field",
+                    ControllField = string.Empty
+                };
+
+            var totalRecords = query.Count();
+            return new {
+                total = Convert.ToInt32(Math.Ceiling((double) totalRecords/rows)),
+                page = page,
+                records = totalRecords,
+                rows = query
+            };
+        }
+
+        // DELETE api/metadata/field/name
+        public virtual HttpResponseMessage Delete([FromUri] string[] name, string entityName) {
+            var state = true;
+            foreach (var n in name) {
+                var stateService = _contentMetadataService.DeleteField(n, entityName);
+                state = state && stateService;
+            }
+            return state
+                ? Request.CreateResponse(HttpStatusCode.OK)
+                : Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid id!");
+        }
+    }
+}
