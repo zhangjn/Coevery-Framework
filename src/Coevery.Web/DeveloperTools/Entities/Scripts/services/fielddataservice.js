36b45b4f9fd3360a51c6c5e4e1c9803b3604d67a
Zhang Haoran <icese7en@gmail.com>
2014-03-12 13:32:57 +0800

    Fix the error that field can't delete

--- src/Coevery.Web/DeveloperTools/Entities/Scripts/services/fielddataservice.js
+++ src/Coevery.Web/DeveloperTools/Entities/Scripts/services/fielddataservice.js
@@ -4,7 +4,7 @@ define(['core/app/detourService'], function(detour) {
     detour.registerFactory([
         'fieldDataService',
         ['$rootScope', '$resource', function($rootScope, $resource) {
-            return $resource(applicationBaseUrl + 'api/developertools/field/:Name',
+            return $resource(applicationBaseUrl + 'api/entities/field/:Name',
                 { Name: '@Name' },
                 { update: { method: 'PUT' } });
         }]
