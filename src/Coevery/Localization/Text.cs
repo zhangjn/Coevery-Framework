da736379cbd89629090b395d7c9558b023e8f232
sword <caoxiangkun@gmail.com>
2014-03-14 10:17:40 +0800

    fix localization format issue

--- src/Coevery/Localization/Text.cs
+++ src/Coevery/Localization/Text.cs
@@ -24,17 +24,19 @@ namespace Coevery.Localization {
             Logger.Debug("{0} localizing '{1}'", _scope, textHint);
 
             var workContext = _workContextAccessor.GetContext();
-	        
-	        if (workContext != null) {
-		        var currentCulture = workContext.CurrentCulture;
-		        var localizedFormat = _localizedStringManager.GetLocalizedString(_scope, textHint, currentCulture);
 
-				return args.Length == 0
-				? new LocalizedString(localizedFormat, _scope, textHint, args)
-				: new LocalizedString(string.Format(GetFormatProvider(currentCulture), localizedFormat, args), _scope, textHint, args);
-	        }
+            if (workContext != null) {
+                var currentCulture = workContext.CurrentCulture;
+                var localizedFormat = _localizedStringManager.GetLocalizedString(_scope, textHint, currentCulture);
 
-			return new LocalizedString(textHint, _scope, textHint, args);
+                return args.Length == 0
+                    ? new LocalizedString(localizedFormat, _scope, textHint, args)
+                    : new LocalizedString(string.Format(GetFormatProvider(currentCulture), localizedFormat, args), _scope, textHint, args);
+            }
+
+            return args.Length == 0
+                ? new LocalizedString(textHint, _scope, textHint, args)
+                : new LocalizedString(string.Format(textHint, args), _scope, textHint, args);
         }
 
         private static IFormatProvider GetFormatProvider(string currentCulture) {
