7d69ef838659b817bfbbfbafb8e5c65d4af7e764
Zhang Haoran <icese7en@gmail.com>
2014-03-14 17:02:52 +0800

    Commit front-end support

--- src/Coevery.Web/Core/Common/Services/SafeModeThemeSelector.cs
+++ src/Coevery.Web/Core/Common/Services/SafeModeThemeSelector.cs
@@ -1,12 +1,18 @@
 ﻿using System.Web.Routing;
 using Coevery.Themes;
+using Coevery.UI.Admin;
 using JetBrains.Annotations;
 
 namespace Coevery.Core.Common.Services {
     [UsedImplicitly]
     public class SafeModeThemeSelector : IThemeSelector {
         public ThemeSelectorResult GetTheme(RequestContext context) {
-            return new ThemeSelectorResult {Priority = -100, ThemeName = "Themes"};
+            if (!AdminFilter.IsApplied(context))
+            {
+                return new ThemeSelectorResult { Priority = -100, ThemeName = "Mooncake" };
+            }
+            return null;
         }
+
     }
 }
