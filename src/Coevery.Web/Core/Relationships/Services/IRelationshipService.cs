3fb7a567146f3e7dad86d74f2f2f7a4f90e29d60
Zhang Junnan <zhangjn0430@gmail.com>
2014-03-14 15:48:47 +0800

    Remove unused codes

--- src/Coevery.Web/Core/Relationships/Services/IRelationshipService.cs
+++ src/Coevery.Web/Core/Relationships/Services/IRelationshipService.cs
@@ -1,11 +1,7 @@
-﻿using System.Web.Mvc;
-using Coevery.Core.Relationships.Records;
+﻿using Coevery.Core.Relationships.Records;
 
 namespace Coevery.Core.Relationships.Services {
     public interface IRelationshipService : IDependency {
-        string CheckRelationName(string name);
-        SelectListItem[] GetEntityNames(string excludeEntity);
-        SelectListItem[] GetFieldNames(string entityName);
         RelationshipRecord[] GetRelationships(string entityName);
         OneToManyRelationshipRecord GetOneToMany(int id);
         ManyToManyRelationshipRecord GetManyToMany(int id);
--- src/Coevery.Web/Core/Relationships/Services/RelationshipService.cs
+++ src/Coevery.Web/Core/Relationships/Services/RelationshipService.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Web.Mvc;
 using Coevery.ContentManagement;
 using Coevery.ContentManagement.MetaData;
 using Coevery.Core.Common.Extensions;
@@ -7,17 +6,13 @@ using Coevery.Core.Entities.Models;
 using Coevery.Core.Relationships.Records;
 using Coevery.Core.Relationships.Settings;
 using Coevery.Data;
-using Coevery.Utility.Extensions;
 
 namespace Coevery.Core.Relationships.Services {
     public class RelationshipService : IRelationshipService {
-        #region Class definition
-
         private readonly IRepository<RelationshipRecord> _relationshipRepository;
         private readonly IRepository<OneToManyRelationshipRecord> _oneToManyRepository;
         private readonly IRepository<ManyToManyRelationshipRecord> _manyToManyRepository;
 
-        private readonly IContentDefinitionExtension _contentDefinitionExtension;
         private readonly IContentDefinitionManager _contentDefinitionManager;
         private readonly IContentManager _contentManager;
 
@@ -25,7 +20,6 @@ namespace Coevery.Core.Relationships.Services {
             IRepository<RelationshipRecord> relationshipRepository,
             IRepository<OneToManyRelationshipRecord> oneToManyRepository,
             IRepository<ManyToManyRelationshipRecord> manyToManyRepository,
-            IContentDefinitionExtension contentDefinitionExtension,
             IContentDefinitionManager contentDefinitionManager,
             IContentManager contentManager) {
             _relationshipRepository = relationshipRepository;
@@ -33,22 +27,6 @@ namespace Coevery.Core.Relationships.Services {
             _manyToManyRepository = manyToManyRepository;
             _contentDefinitionManager = contentDefinitionManager;
             _contentManager = contentManager;
-            _contentDefinitionExtension = contentDefinitionExtension;
-        }
-
-        #endregion
-
-        #region GetMethods
-
-        public string CheckRelationName(string name) {
-            string errorMessage = null;
-            if (!string.Equals(name, name.ToSafeName())) {
-                errorMessage += "The name:\"" + name + "\" is not legal!\n";
-            }
-            if (_relationshipRepository.Fetch(relation => relation.Name == name).FirstOrDefault() != null) {
-                errorMessage += "The name:\"" + name + "\" already exists!\n";
-            }
-            return errorMessage;
         }
 
         public string GetReferenceField(string entityName, string relationName) {
@@ -59,30 +37,6 @@ namespace Coevery.Core.Relationships.Services {
             return reference == null ? null : reference.Name;
         }
 
-        public SelectListItem[] GetFieldNames(string entityName) {
-            var entity = _contentDefinitionManager.GetPartDefinition(entityName.ToPartName());
-            return entity == null
-                ? null
-                : (from field in entity.Fields
-                    select new SelectListItem {
-                        Value = field.Name,
-                        Text = field.DisplayName,
-                        Selected = false
-                    }).ToArray();
-        }
-
-        public SelectListItem[] GetEntityNames(string excludeEntity) {
-            var entities = _contentDefinitionExtension.ListUserDefinedTypeDefinitions();
-            return entities == null
-                ? null
-                : (from entity in entities
-                    where entity.Name != excludeEntity
-                    select new SelectListItem {
-                        Value = entity.Name,
-                        Text = entity.DisplayName,
-                        Selected = false
-                    }).ToArray();
-        }
 
         public OneToManyRelationshipRecord GetOneToMany(int id) {
             return _oneToManyRepository.Get(record => record.Relationship.Id == id);
@@ -106,7 +60,5 @@ namespace Coevery.Core.Relationships.Services {
                       && (record.PrimaryEntity == entity.Record || record.RelatedEntity == entity.Record)
                 select record).ToArray();
         }
-
-        #endregion
     }
 }
