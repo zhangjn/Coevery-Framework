1babc9c6e2ca01cab7c3c11685a6a83524aa1960
Zhang Junnan <zhangjn0430@gmail.com>
2014-03-19 13:22:10 +0800

    Add Template

--- src/Coevery.Web/DeveloperTools/FormDesigner/Controllers/LayoutController.cs
+++ src/Coevery.Web/DeveloperTools/FormDesigner/Controllers/LayoutController.cs
@@ -1,25 +1,14 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
 using Coevery.ContentManagement.MetaData;
-using Coevery.ContentManagement.MetaData.Models;
-using Coevery.Core.Common.Extensions;
-using Coevery.DeveloperTools.FormDesigner.Models;
-using Coevery.DeveloperTools.FormDesigner.Services;
 
 namespace Coevery.DeveloperTools.FormDesigner.Controllers {
     public class LayoutController : ApiController {
         private readonly IContentDefinitionManager _contentDefinitionManager;
-        private readonly ITemplateViewService _templateViewService;
-        private const string AlwaysInLayoutKey = "TextFieldSettings.AlwaysInLayout";
 
-        public LayoutController(IContentDefinitionManager contentDefinitionManager,
-            ITemplateViewService templateViewService) {
+        public LayoutController(IContentDefinitionManager contentDefinitionManager) {
             _contentDefinitionManager = contentDefinitionManager;
-            _templateViewService = templateViewService;
         }
 
         // GET api/leads/lead/5
@@ -41,7 +30,7 @@ namespace Coevery.DeveloperTools.FormDesigner.Controllers {
         }
 
         // POST api/metadata/field
-        public virtual HttpResponseMessage Post(string id, ICollection<Section> data) {
+        public virtual HttpResponseMessage Post(string id, Data data) {
             if (!ModelState.IsValid) {
                 return Request.CreateResponse(HttpStatusCode.MethodNotAllowed);
             }
@@ -51,47 +40,18 @@ namespace Coevery.DeveloperTools.FormDesigner.Controllers {
                 return Request.CreateResponse(HttpStatusCode.NotFound);
             }
 
-            var layout = GetLayout(contentTypeDefinition, data);
-            if (string.IsNullOrEmpty(layout)) {
-                return Request.CreateResponse(HttpStatusCode.ExpectationFailed);
-            }
             if (contentTypeDefinition.Settings.ContainsKey("Layout")) {
-                contentTypeDefinition.Settings["Layout"] = layout;
+                contentTypeDefinition.Settings["Layout"] = data.Layout;
             }
             else {
-                contentTypeDefinition.Settings.Add("Layout", layout);
+                contentTypeDefinition.Settings.Add("Layout", data.Layout);
             }
             _contentDefinitionManager.StoreTypeDefinition(contentTypeDefinition);
             return Request.CreateResponse(HttpStatusCode.OK);
         }
+    }
 
-        private string GetLayout(ContentTypeDefinition contentTypeDefinition, IEnumerable<Section> data) {
-            //check field valid
-            if (contentTypeDefinition.Parts.Any()) {
-                var part = contentTypeDefinition.Parts.First(x => x.PartDefinition.Name == contentTypeDefinition.Name.ToPartName());
-                var partFields = part.PartDefinition.Fields.ToList();
-                var fields = data.SelectMany(x => x.Rows)
-                    .SelectMany(x => x.Columns)
-                    .Where(x => x.Field != null)
-                    .Select(x => x.Field).ToList();
-                if (partFields.Any(f => f.Settings.ContainsKey(AlwaysInLayoutKey)
-                                        && bool.Parse(f.Settings[AlwaysInLayoutKey])
-                                        && !fields.Select(x => x.FieldName).Contains(f.Name))) {
-                    return string.Empty;
-                }
-                foreach (var field in fields) {
-                    if (partFields.Any(x => x.Name == field.FieldName)) {
-                        field.IsValid = true;
-                    } else if (contentTypeDefinition.Parts.Any(x => x.PartDefinition.Name == field.FieldName)) {
-                        field.IsValid = true;
-                    }
-                }
-            }
-
-            var viewData = new ViewDataDictionary {{"Layout", data}};
-            string layout = _templateViewService
-                .RenderView("FormDesigner", "FormTemplate", "FormDesignerLayout", viewData);
-            return layout;
-        }
+    public class Data {
+        public string Layout { get; set; }
     }
 }
