50b064151b41be0cec3af79237946c18fae082e4
Zhang Junnan <zhangjn0430@gmail.com>
2014-03-20 14:25:40 +0800

    Remove unused code

--- src/Coevery.Web/DeveloperTools/FormDesigner/Controllers/AdminController.cs
+++ src/Coevery.Web/DeveloperTools/FormDesigner/Controllers/AdminController.cs
@@ -3,27 +3,23 @@ using System.Linq;
 using System.Net;
 using System.Web.Http;
 using System.Web.Mvc;
-using Coevery.ContentManagement;
 using Coevery.ContentManagement.MetaData;
 using Coevery.Core.Common.Extensions;
 using Coevery.DeveloperTools.EntityManagement.Services;
 using Coevery.DeveloperTools.FormDesigner.Models;
 using Coevery.DeveloperTools.FormDesigner.ViewModels;
 using Coevery.Localization;
-using Coevery.Logging;
 using Newtonsoft.Json;
 
 namespace Coevery.DeveloperTools.FormDesigner.Controllers {
     public class AdminController : Controller {
-        private readonly IContentManager _contentManager;
         private readonly IContentDefinitionManager _contentDefinitionManager;
         private readonly IContentMetadataService _contentMetadataService;
 
         public AdminController(
             ICoeveryServices coeveryServices,
             IContentMetadataService contentMetadataService,
-            IContentManager contentManager, IContentDefinitionManager contentDefinitionManager) {
-            _contentManager = contentManager;
+            IContentDefinitionManager contentDefinitionManager) {
             _contentDefinitionManager = contentDefinitionManager;
             _contentMetadataService = contentMetadataService;
             Services = coeveryServices;
@@ -32,7 +28,6 @@ namespace Coevery.DeveloperTools.FormDesigner.Controllers {
 
         public ICoeveryServices Services { get; private set; }
         public Localizer T { get; set; }
-        public ILogger Logger { get; set; }
 
         public ActionResult Index(string id, string returnUrl) {
             if (string.IsNullOrEmpty(id)) {
@@ -41,8 +36,8 @@ namespace Coevery.DeveloperTools.FormDesigner.Controllers {
             if (!_contentMetadataService.CheckEntityPublished(id)) {
                 return Content(T("The \"{0}\" hasn't been published!", id).Text);
             }
-            var contentItem = _contentManager.New(id);
-            dynamic model = _contentManager.BuildEditor(contentItem);
+            var contentItem = Services.ContentManager.New(id);
+            dynamic model = Services.ContentManager.BuildEditor(contentItem);
             var contentTypeDefinition = contentItem.TypeDefinition;
 
             var viewModel = Services.New.ViewModel();
--- src/Coevery.Web/DeveloperTools/FormDesigner/Controllers/LayoutController.cs
+++ src/Coevery.Web/DeveloperTools/FormDesigner/Controllers/LayoutController.cs
@@ -11,24 +11,6 @@ namespace Coevery.DeveloperTools.FormDesigner.Controllers {
             _contentDefinitionManager = contentDefinitionManager;
         }
 
-        // GET api/leads/lead/5
-        public virtual object Get(string id) {
-            var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(id);
-            if (contentTypeDefinition == null) {
-                return Request.CreateResponse(HttpStatusCode.NotFound);
-            }
-
-            string layout = contentTypeDefinition.Settings.ContainsKey("Layout")
-                ? contentTypeDefinition.Settings["Layout"]
-                : null;
-
-            var layoutInfo = new {
-                id,
-                layout
-            };
-            return layoutInfo;
-        }
-
         // POST api/metadata/field
         public virtual HttpResponseMessage Post(string id, Data data) {
             if (!ModelState.IsValid) {
