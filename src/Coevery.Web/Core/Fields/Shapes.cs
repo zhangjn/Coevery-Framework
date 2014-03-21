90177da6a71c364052f1478e445df67129bed051
Zhang Junnan <zhangjn0430@gmail.com>
2014-03-21 16:22:39 +0800

    Refactor display field

--- src/Coevery.Web/Core/Fields/Shapes.cs
+++ src/Coevery.Web/Core/Fields/Shapes.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Coevery.ContentManagement;
@@ -30,53 +31,58 @@ namespace Coevery.Core.Fields {
         public void DisplayField(ContentPart part, string fieldName, dynamic Display, TextWriter Output) {
             var shapeFactory = _shapeFactory.Value;
 
-            dynamic itemShape = shapeFactory.Create("Content", Arguments.Empty(), () => new ZoneHolding(() => shapeFactory.Create("ContentZone", Arguments.Empty())));
-            itemShape.ContentItem = part.ContentItem;
-
+            dynamic itemShape = GetContentShape(shapeFactory, part);
             var context = new BuildDisplayContext(itemShape, part, "", string.Empty, shapeFactory);
             context.FindPlacement = (partShapeType, differentiator, defaultLocation) => new PlacementInfo {Location = "Content"};
 
-            var field = part.Fields.FirstOrDefault(f => f.Name == fieldName);
-            if (field != null) {
-                var fieldTypeName = field.FieldDefinition.Name;
-                var drivers = _contentFieldDrivers.Where(x => x.Value.GetFieldInfo().Any(fi => fi.FieldTypeName == fieldTypeName)).ToList();
-                drivers.Invoke(driver => {
-                    context.Logger = Logger;
-                    var result = driver.Value.BuildDisplayShape(context) as CombinedResult;
-                    if (result != null) {
-                        var driverResults = result.GetResults().Where(x => x.ContentField == field);
-                        foreach (var driverResult in driverResults) {
-                            driverResult.Apply(context);
-                        }
+            Action<Work<IContentFieldDriver>, ContentField, BuildDisplayContext> buildAction = (driver, field, buildContext) => {
+                var result = driver.Value.BuildDisplayShape(buildContext) as CombinedResult;
+                if (result != null) {
+                    var driverResults = result.GetResults().Where(x => x.ContentField == field);
+                    foreach (var driverResult in driverResults) {
+                        driverResult.Apply(buildContext);
                     }
-                }, Logger);
-                Output.Write(Display(context.Shape));
-            }
+                }
+            };
+
+            DisplayFieldShape(part, fieldName, context, buildAction, Display, Output);
         }
 
         [Shape]
         public void DisplayFieldEditor(ContentPart part, string fieldName, dynamic Display, TextWriter Output) {
             var shapeFactory = _shapeFactory.Value;
 
-            dynamic itemShape = shapeFactory.Create("Content", Arguments.Empty(), () => new ZoneHolding(() => shapeFactory.Create("ContentZone", Arguments.Empty())));
-            itemShape.ContentItem = part.ContentItem;
-
+            dynamic itemShape = GetContentShape(shapeFactory, part);
             var context = new BuildEditorContext(itemShape, part, "", string.Empty, shapeFactory);
             context.FindPlacement = (partShapeType, differentiator, defaultLocation) => new PlacementInfo {Location = "Content"};
 
+            Action<Work<IContentFieldDriver>, ContentField, BuildEditorContext> buildAction = (driver, field, buildContext) => {
+                var result = driver.Value.BuildEditorShape(buildContext) as CombinedResult;
+                if (result != null) {
+                    var driverResults = result.GetResults().Where(x => x.ContentField == field);
+                    foreach (var driverResult in driverResults) {
+                        driverResult.Apply(buildContext);
+                    }
+                }
+            };
+
+            DisplayFieldShape(part, fieldName, context, buildAction, Display, Output);
+        }
+
+        private dynamic GetContentShape(IShapeFactory shapeFactory, ContentPart part) {
+            dynamic shape = shapeFactory.Create("Content", Arguments.Empty(), () => new ZoneHolding(() => shapeFactory.Create("ContentZone", Arguments.Empty())));
+            shape.ContentItem = part.ContentItem;
+            return shape;
+        }
+
+        private void DisplayFieldShape<TContext>(ContentPart part, string fieldName, TContext context, Action<Work<IContentFieldDriver>, ContentField, TContext> buildAction, dynamic Display, TextWriter Output) where TContext : BuildShapeContext {
             var field = part.Fields.FirstOrDefault(f => f.Name == fieldName);
             if (field != null) {
                 var fieldTypeName = field.FieldDefinition.Name;
                 var drivers = _contentFieldDrivers.Where(x => x.Value.GetFieldInfo().Any(fi => fi.FieldTypeName == fieldTypeName)).ToList();
                 drivers.Invoke(driver => {
                     context.Logger = Logger;
-                    var result = driver.Value.BuildEditorShape(context) as CombinedResult;
-                    if (result != null) {
-                        var driverResults = result.GetResults().Where(x => x.ContentField == field);
-                        foreach (var driverResult in driverResults) {
-                            driverResult.Apply(context);
-                        }
-                    }
+                    buildAction(driver, field, context);
                 }, Logger);
                 Output.Write(Display(context.Shape));
             }
