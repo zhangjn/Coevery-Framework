e6be4ea71b315264cfe4f123d6d935ebfecd0912
Zhang Junnan <zhangjn0430@gmail.com>
2014-03-24 16:14:58 +0800

    Add Mooncake

--- src/Coevery.Web/Core/Common/Services/SafeModeThemeSelector.cs
+++ src/Coevery.Web/Core/Common/Services/SafeModeThemeSelector.cs
@@ -6,7 +6,7 @@ namespace Coevery.Core.Common.Services {
     [UsedImplicitly]
     public class SafeModeThemeSelector : IThemeSelector {
         public ThemeSelectorResult GetTheme(RequestContext context) {
-            return new ThemeSelectorResult {Priority = -100, ThemeName = "Themes"};
+            return new ThemeSelectorResult {Priority = -100, ThemeName = "Mooncake"};
         }
     }
 }
